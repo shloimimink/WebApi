c5796736b146999c45a79646385e5066bc2eed43
Shlomo Minkowicz <shloimimink@gmail.com>
2020-07-14 22:05:46 -0500

    initial commit

---
+++ WebApi/Controllers/DepartmentController.cs
@@ -0,0 +1,114 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using WebApi.Models;
+using System;
+
+namespace WebApi.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+
+            DataTable table = new DataTable();
+
+            string query = @"
+                           select DepartmentID, DepartmentName from
+                           dbo.Department
+                           ";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da =  new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+            
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+
+        }
+
+        public string Post(Department dep)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                          insert into dbo.Department values ('" + dep.DepartmentName + @"')
+                           ";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added Successfully";
+            }
+            catch (Exception)
+            {
+                return "Failed to Add";
+            }
+        }
+
+        public string Put(Department dep)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                 update dbo.Department set DepartmentName = '" + dep.DepartmentName + @"'
+                        where DepartmentID = " + dep.DepartmentID + @"
+                           ";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Updated Successfully";
+            }
+            catch (Exception)
+            {
+                return "Failed to Updated";
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                 delete from dbo.Department where DepartmentID = " + id;
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Deleted Successfully";
+            }
+            catch (Exception)
+            {
+                return "Failed to Delete";
+            }
+        }
+    }
+}
